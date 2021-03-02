79af1a3265b46a3dd429ba92d78fdbd1ac47fd9d
pauga <pau.galan22@gmail.com>
2021-03-02 20:16:06 +0100

    (COMPLETADO) 5 Most Retweeted Tweets for Most Retweeted Users

--- src/main/java/upf/edu/MostRetweetedApp.java
+++ src/main/java/upf/edu/MostRetweetedApp.java
@@ -1,19 +1,15 @@
 package upf.edu;
 
-import com.amazonaws.jmespath.OpGreaterThan;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.io.FileWriter;
-import java.io.IOException;
 
 
 //spark-submit --class upf.edu.MostRetweetedApp --master local[*] target/lab2-4-1.0-SNAPSHOT.jar ./dataset/out/ ./dataset/Eurovision9.json
@@ -23,8 +19,6 @@ public class MostRetweetedApp {
         List<String> argsList = Arrays.asList(args);
         String outputDir = argsList.get(0);
         String input = argsList.get(1);
-
-
         long start = System.currentTimeMillis();
 
         SparkConf conf = new SparkConf().setAppName("Twitter Filter");
@@ -34,9 +28,9 @@ public class MostRetweetedApp {
         JavaRDD<ExtendedSimplifiedTweet> sentences = sparkContext.textFile(input)
                 .filter(sentence -> !sentence.isEmpty())
                 .map(word -> ExtendedSimplifiedTweet.fromJson(word)) // Get Optional from tweet json
-                .filter(s-> s.isPresent()) // Filter only present
+                .filter(s -> s.isPresent()) // Filter only present
                 .filter(s -> s.get().getIsRetweeted())
-                .map(s-> s.get());
+                .map(s -> s.get());
 
 
         //Usuarios ordenador de mas a menos retweets
@@ -52,7 +46,7 @@ public class MostRetweetedApp {
         //10 tweets con mas retweets
         List<Tuple2<Integer, String>> aux = swapped1.take(10);
         JavaPairRDD<Integer, String> TenMostRetUsers = sparkContext.parallelizePairs(aux, 1);
-        List<String> topUsers = TenMostRetUsers.map(x->x._2).take(10);
+        List<String> topUsers = TenMostRetUsers.map(x -> x._2).take(10);
         List<Tuple2<String, String>> topUsersTweets;
 
         //Lista de los tweets de los 10 usuarios mas retweeteados
@@ -63,12 +57,20 @@ public class MostRetweetedApp {
 
 
         //Cuantos retweets tiene el tweet más retweeteado de los 10 usuarios mas retweeteados
-        JavaPairRDD<Integer, String> swapped2 = countsTweets
-                .mapToPair(s-> new Tuple2<>(s._2, s._1._1))
-                .sortByKey(Math::max)
+        JavaPairRDD<String, Integer> maxRetweets = countsTweets
+                .mapToPair(s -> new Tuple2<>(s._1._1, s._2))
+                .reduceByKey((a, b) -> Math.max(a, b));
+
+        List<Tuple2<String, Integer>> maxRetweetsUser = maxRetweets.collect();
+
+        //Busqueda final
+        JavaPairRDD<Integer, Tuple2<String, String>> result = countsTweets
+                .mapToPair(x -> new Tuple2<>(x._2, new Tuple2<>(x._1._1, x._1._2)))
+                .filter(y -> topUsers.contains(y._2._1))
+                .filter(z -> maxRetweetsUser.contains(new Tuple2<>(z._2._1, z._1)))
                 .sortByKey(false);
 
-        swapped2.repartition(1).saveAsTextFile(outputDir);
+        result.repartition(1).saveAsTextFile(outputDir);
     }
 
     private static String normalise(String word) {
